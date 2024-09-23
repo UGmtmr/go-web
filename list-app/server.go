c6ee257d18925b62e25b87781ac4f3b0e977ce94
DESKTOP-EDF04IQ\yuji <matsumaro.yuji@fois-gp.com>
2024-09-23 21:57:10 +0900

    テキストファイルを読み込んでHTMLを動的に書き換えました！

--- list-app/server.go
+++ list-app/server.go
@@ -1,24 +1,53 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"html/template"
 	"log"
 	"net/http"
+	"os"
 )
-
-func viewHandler(w http.ResponseWriter, r *http.Request) {
-	html, err := template.ParseFiles("view.html")
-	if err != nil {
-		log.Fatal(err)
-	}
-	if err := html.Execute(w, nil); err != nil {
-		log.Fatal(err)
-	}
-}
 
 func main() {
 	http.HandleFunc("/view", viewHandler)
 	fmt.Println("Server Start Up...........")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+}
+
+func fileRead(fileName string) []string {
+	var bookList []string
+	file, err := os.Open(fileName)
+	if os.IsNotExist(err) {
+		return nil
+	}
+
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		bookList = append(bookList, scanner.Text())
+	}
+	return bookList
+}
+
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	bookList := fileRead("reading.txt")
+	html, err := template.ParseFiles("view.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	getBooks := New(bookList)
+
+	if err := html.Execute(w, getBooks); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type BookList struct {
+	Books []string
+}
+
+func New(books []string) *BookList {
+	return &BookList{Books: books}
 }
